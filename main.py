14e04f6375227f40c1251006e1efa309cfae4577
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-09 23:34:39 -0300

    Feat: Selecting components with selenium

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+driver.get("https://python.org")
 
-# driver.quit()
+# price_dollar = driver.find_element(By.CLASS_NAME, "apexPriceToPay")
+# price_cents = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+# search_bar = driver.find_element(By.NAME, "q")
+# print(search_bar.get_attribute("placeholder"))
+#
+#
+# documentation_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
+# print(documentation_link.text)
+#
+#
+# button = driver.find_element(By.ID, "submit")
+# print(button.size)
+# bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+
+driver.quit()
